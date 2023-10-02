57ac3a5f61a2b2ce3797996bb93341acb3494093
Semih Kim <semih.kim@gmail.com>
2023-10-02 21:17:09 +0900

    Fix add example: address is fixed

--- examples/add.asm
+++ examples/add.asm
@@ -5,33 +5,38 @@ def init(host):
     host.data[0x200800] = host.zeros(512 * 2)
 
     host.store(0, 0x00, 0x00)  # 0x00 means kernel
-    host.store(0, 0x100, 0x200000, 512 * 2)
-    host.store(0, 0x500, 0x200400, 512 * 2)
+    host.store(0, 0x80, 0x200000, 512 * 2)
+    host.store(0, 0x480, 0x200400, 512 * 2)
 
 def run(host):
     host.exec(0)
 
 def finalize(host):
-    #host.dump_bf16(0x200800)
-    #host.dump(0x200800)
-    #host.npus[0].dump_bf16()
-    host.npus[0].dump_regs()
+    #host.npus[0].dump_regs()
+    host.dump_bf16(0x200800)
+    #print('A')
+    #host.npus[0].dump_bf16(0x80, 512)
+    #print('B')
+    #host.npus[0].dump_bf16(0x480, 512)
+    #print('C')
+    #host.npus[0].dump_bf16(0x880, 512)
 ###
 
-# A is stored at 0x200
-seti %a 0x80  # 0x200 / 4
-# B is stored at 0x600
-seti %b 0x180  # 0x600 / 4
-# C is stored at 0xa00
-seti %c 0x280  # 0xa00 / 4
+# kernel is stored at 0
+# A is stored at 128
+seti %a 0x20  # 128 / 4
+# B is stored at 128 + 1024
+seti %b 0x120  # (128 + 1024) / 4
+# C is stored at 128 + 1024 + 1024
+seti %c 0x220  # (128 + 1024) / 4
 # count is 512
 seti %d 512
 
 vadd.bf16 %c %a %b %d
 
 # Store C to 0x200800
-seti %a 0x410  # 0x200800 / 128
-seti %b 0x280  # 0xa00 / 4
+seti %a 0x4010  # 0x200800 / 128
+seti %b 0x220  # (128 + 1024 + 1024) / 4
 seti %c 256  # (512 * 2) / 4
 store %a %b %c
 
